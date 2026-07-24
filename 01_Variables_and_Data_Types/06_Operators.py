b861ff798765e31c769a136ed7b9fb73ec61b50a
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-07-24 22:44:57 +0530

    Add files via upload

---
+++ 01_Variables_and_Data_Types/06_Operators.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) # remainder
+print(a ** b) # a raise to the power b
+
+# Relational Operators
+a = 50
+b = 20
+print(a == b) # False
+print(a != b) # True
+print(a >= b) # True
+print(a > b) # True
+print(a <= b) # False
+print(a < b) # False
+
+# Assignment Operator
+num = 10
+num = num + 10
+print(num)
+num += 10
+print (num)
+num -= 10
+print(num)
+num *= 10
+print(num)
+num /= 10
+print(num)
+
+# Logical Operators
+print(not False)
+print(not True)
+
+val1 = True
+val2 = True
+print("AND operator:", val1 and val2)
+print("OR operator:", val1 or val2)
