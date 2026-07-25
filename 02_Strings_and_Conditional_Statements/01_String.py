4a3b5f2416a7039439873e633a4a3281547afdde
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-07-25 22:28:09 +0530

    Add files via upload

---
+++ 02_Strings_and_Conditional_Statements/01_String.py
@@ -0,0 +1,12 @@
+str1 = "This is a string.\nWe are creating in python"
+str2 = 'ApnaCollege'
+str3 = """This is a string"""
+
+print(str1)
+print(str2)
+print(str3)
+print(len(str3)) # to calculate length of string we use len(str)
+
+str4 = "Apna"
+str5 = "College"
+print(str4 + str5)
